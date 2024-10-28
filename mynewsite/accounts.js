62c87e2cc935709713791a77fefede4ef867f445
vcloud24 <vilakazigames@gmail.com>
2024-10-28 10:29:25 +0200

    Update accounts.js

--- mynewsite/accounts.js
+++ mynewsite/accounts.js
@@ -1,12 +1,26 @@
-const accounts = [];
+const accounts = {};
 
-function addAccount(username) {
-    if (accounts.includes(username)) {
+function addAccount(username, password, birthYear, ip) {
+    const currentYear = new Date().getFullYear();
+    const age = currentYear - birthYear;
+
+    if (accounts[username]) {
         return "Username is taken";
-    } else {
-        accounts.push(username);
-        return "Account created";
     }
+    
+    if (age < 8) {
+        return "You must be at least 8 years old to create an account";
+    }
+
+    accounts[username] = {
+        password: password,
+        ip: ip,
+        folder: {
+            username: username,
+            password: password,
+            ip: ip
+        }
+    };
+
+    return "Account created";
 }
-
-
